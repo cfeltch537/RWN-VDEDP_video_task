9806101df6f165d5ec98a7a9ab1346e2c8ce1cbf
Matt O'Donnell <mbod@ASCs-MacBook-Pro.local>
2017-01-05 14:10:06 -0500

    first commit

--- video_task.py
+++ video_task.py
@@ -13,50 +13,74 @@ from __future__ import print_function, division
 
 from psychopy import visual, logging, core, event
 
-
+DEBUG=True
+use_fullscreen = False
 
 globalClock = core.Clock()
-logging.console.setLevel(logging.DATA)
+localClock = core.Clock()
 
-logger = logging.LogFile('logs/test.log')
+#logging.console.setLevel(logging.DEBUG)
+
+logger = logging.LogFile('logs/test.log', filemode='w', level=logging.DEBUG)
 
 
 #############################
 # set up window and stimuli #
 #############################
 
-win = visual.Window((1280,720))
+win = visual.Window([1200,800],  
+                monitor="testMonitor", 
+                fullscr=use_fullscreen, 
+                units="deg")
+                
+win.setRecordFrameIntervals(False)  
+
 
 video = visual.MovieStim3(win, filename='stimuli/test1.mp4',
                              pos=(0,0),
                              size=(1024,576),
-                             flipVert=False, flipHoriz=False, 
-                             loop=False)
-                             
+                             flipVert=False, flipHoriz=False, loop=False)                           
 
 
-fixation = visual.TextStim(win, text="+", pos=[0,0], height=1)
+fixation = visual.TextStim(win, text="+", pos=[0,0], height=2)
 
 
-fixation.draw()
-win.flip()
-core.wait(2.0)
 
-# play video
-should_flip = True # video.play()
 
-while video.status != visual.FINISHED:
+def play_video(filename):
     
-    if should_flip:
+    video.loadMovie(filename)
+    localClock.reset()
+    video_duration = video.duration
+
+    if DEBUG:
+        print("Video {} duration is {}".format(filename,video_duration))
+
+    # play video
+    video.play()
+
+    while localClock.getTime() < video_duration:
+        
         video.draw()
         win.flip()
-    if event.getKeys():
-        break
 
-logger.flush()
+        if event.getKeys(['escape']):
+            break
 
-# clean up
-win.close()
-core.quit()
+    logging.flush()
+
+
+
+if __name__ == '__main__':
+
+    fixation.draw()
+    win.flip()
+    core.wait(2.0)
+
+    play_video('stimuli/test1.mp4')
+
+    # clean up
+    win.close()
+    core.quit()
 
 
