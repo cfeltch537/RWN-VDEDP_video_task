3c73fdc8d5488f45f9c836096754265121c831c6
Cody Feltch <cfeltch537@gmail.com>
2017-02-15 11:42:29 -0500

    Updated to matts video_task.py since merge kept failing

--- video_task.py
+++ video_task.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 #################################
@@ -7,21 +6,63 @@
 # video task
 #
 # ver 0.1 - mbod@asc.upenn.edu 11/20/16
+# ver 0.11 - mbod@asc.upenn.edu 2/13/17
 #
 
 from __future__ import print_function, division
 
 from psychopy import visual, logging, core, event
 
+# -- set the order of audio libraries
+# -- need pygame first for movie audio
+from psychopy import prefs
+prefs.general['audioLib'] = ['pygame','pyo']
+
+
+import os
+import re
+import time
+
+##########
+# PARAMS #
+##########
+
+STIM_DIR = 'stimuli'
+
+
 DEBUG=True
 use_fullscreen = False
+
+
+#############################
+# set up clocks and logging #
+#############################
 
 globalClock = core.Clock()
 localClock = core.Clock()
 
-#logging.console.setLevel(logging.DEBUG)
+if not os.path.exists('logs'):
+    os.mkdir('logs')
 
-logger = logging.LogFile('logs/test.log', filemode='w', level=logging.DEBUG)
+
+logging.LogFile('logs/test.log', level=logging.DATA, filemode='w')
+logging.setDefaultClock(globalClock)
+
+    
+
+instructions = ['''In this task, you are going to watch a series of video clips 
+                 of two people who are auditioning for a news anchor position. 
+                  ###
+                  The local news station auditioning the news anchors is trying a 
+                  new format. They want to see if combining humor with regular 
+                  news stories will change how viewers respond to the news.''', 
+                  
+                '''You will see the old and new formats in the news clips.
+                  You will watch 16 clips in total.
+                  ###
+                  Please pay attention to them as you will be asked for your
+                  opinions about them latter.''' ]
+
 
 
 #############################
@@ -35,8 +76,13 @@ win = visual.Window([1200,800],
                 
 win.setRecordFrameIntervals(False)  
 
+ready_prompt = visual.TextStim(win, text='Ready...', pos=(0,2), height=1.3)
 
-video = visual.MovieStim3(win, filename='stimuli/test1.mp4',
+instruction_block = visual.TextStim(win, text='', pos=(0,3), wrapWidth=26, height=1.1)
+
+next_label = visual.TextStim(win, text='[Press space to continue]', pos=(0,-4), height=0.8)
+
+video = visual.MovieStim3(win, filename='stimuli/test1.mov',
                              pos=(0,0),
                              size=(1024,576),
                              flipVert=False, flipHoriz=False, loop=False)                           
@@ -46,6 +92,9 @@ fixation = visual.TextStim(win, text="+", pos=[0,0], height=2)
 
 
 
+#############
+# FUNCTIONS #
+#############
 
 def play_video(filename):
     
@@ -56,11 +105,9 @@ def play_video(filename):
     if DEBUG:
         print("Video {} duration is {}".format(filename,video_duration))
 
-    # play video
-    video.play()
+    logging.log(level=logging.DATA, msg='STARTING video {} dur {}\tsys:{}'.format(filename, video_duration, time.time()))
 
-    while localClock.getTime() < video_duration:
-        
+    while localClock.getTime() < video_duration:        
         video.draw()
         win.flip()
 
@@ -70,17 +117,84 @@ def play_video(filename):
     logging.flush()
 
 
+def do_fixation(fix_length, clock=localClock):
+    
+    
+    logging.log(level=logging.DATA, msg='FIXATION dur {}\tsys:{}'.format(fix_length, time.time()))
+
+    clock.reset()
+    while clock.getTime() < fix_length:
+        fixation.draw()
+        win.flip()
+        
+        
+def show_instructions(display_time=None):
+    # instructions
+    for idx, instruct_text in enumerate(instructions):
+        
+        logging.log(level=logging.DATA, msg='Showing instruction screen {}\tsys:{}'.format(idx+1, time.time()))
+
+        instruct_text = re.sub('^\s+','',
+                                re.sub('###\s*','\n\n',
+                                    re.sub('\s{2,}',' ', instruct_text.strip())))
+        instruction_block.setText(instruct_text)
+        instruction_block.draw()
+        
+        
+        if display_time is None:
+           next_label.draw()
+           win.flip()
+           event.waitKeys(keyList=['space'])
+        else:
+           win.flip()
+           core.wait(display_time)
+           
+        
+
+
+def ready_screen():
+    ready_prompt.draw()
+    next_label.draw()
+    win.flip()
+    event.waitKeys(keyList=['space'])
+    
+    
+    globalClock.reset()
+    logging.log(level=logging.DATA, msg='*********** TASK STARTED *************\tsys:{}'.format(time.time()))
+
+
 
 if __name__ == '__main__':
 
-    fixation.draw()
-    win.flip()
-    core.wait(10.0)
+    ready_screen()
 
-    play_video('stimuli/test1.mp4')
+    show_instructions()
+
+    # TODO - update here with a specific stimuli for experiment sets
+    videos = [vn for vn in os.listdir(STIM_DIR) if vn.endswith('.mov')]
+    videos.sort()
+
+    for vid in videos:
+
+        do_fixation(5.0)
+
+        play_video(os.path.join(STIM_DIR, vid))
+
+
+    do_fixation(10.0)
+
+    logging.log(level=logging.DATA, msg='*********** TASK ENDED *************\tsys:{}'.format(time.time()))
+
 
     # clean up
+    logging.flush()
     win.close()
     core.quit()
 
 
+# --------------------------------------------------------------------------------
+#
+# development note 2/2/17 - some videos throwing error discussed in
+#   https://discourse.psychopy.org/t/error-while-trying-to-play-movie/1253/5
+#   added patch to moviepy/audio/io/readers.py 
+#
